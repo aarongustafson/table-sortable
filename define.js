7c7a5f3131dc57c3bbd206d1f0459a3bda5a7bca
Aaron Gustafson <aaron@easy-designs.net>
2025-12-08 09:10:45 -0800

    Harden define

--- define.js
+++ define.js
@@ -1,3 +1,18 @@
 import { TableSortableElement } from './table-sortable.js';
 
-customElements.define('table-sortable', TableSortableElement);
+export function defineTableSortable(tagName = 'table-sortable') {
+	const hasWindow = typeof window !== 'undefined';
+	const registry = hasWindow ? window.customElements : undefined;
+
+	if (!registry || typeof registry.define !== 'function') {
+		return false;
+	}
+
+	if (!registry.get(tagName)) {
+		registry.define(tagName, TableSortableElement);
+	}
+
+	return true;
+}
+
+defineTableSortable();
